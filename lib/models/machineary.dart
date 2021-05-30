62557cf09ef72c4cc163343aec7214aa050d6c6f
CreamyMilk <creamym254@gmail.com>
2021-05-30 09:40:38 +0300

    Added Machine model

---
+++ lib/models/machineary.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Machinery {
+  final int modelNo;
+  final String heroName;
+  final double price;
+  final int categoryID;
+  final String image;
+  final String packingType;
+  final String name;
+  final String description;
+  final int stock;
+  final bool isBooked;
+  const Machinery(
+      {this.modelNo,
+      @required this.categoryID,
+      this.heroName,
+      this.isBooked = false,
+      this.price,
+      this.image,
+      this.packingType,
+      this.name,
+      this.stock,
+      this.description});
+}
