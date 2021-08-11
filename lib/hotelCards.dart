f566b6efaa50b17aaa23aa936697cd5a9ecf7186
CreamyMilk <creamym254@gmail.com>
2021-08-11 14:04:21 -0400

    ◆ New confirmation card

--- lib/hotelCards.dart
+++ lib/hotelCards.dart
@@ -1,12 +1,9 @@
-
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class HotelListTile extends StatelessWidget {
   const HotelListTile(
-      {Key key,
-   this.prodname, this.imageUrl, this.productID,this.route})
+      {Key key, this.prodname, this.imageUrl, this.productID, this.route})
       : super(key: key);
 
   final String prodname;
@@ -16,130 +13,122 @@ class HotelListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-        return Padding(
-          padding: const EdgeInsets.only(
-              left: 3, right: 3, top: 2, bottom: 2),
-          child: InkWell(
-            onTap: (){
-              
-              Navigator.of(context).pushNamed(route);
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                boxShadow: <BoxShadow>[
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.1),
-                    //offset: const Offset(4, 4),
-                    blurRadius: 32,
-                  ),
-                ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 3, right: 3, top: 2, bottom: 2),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).pushNamed(route);
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                //offset: const Offset(4, 4),
+                blurRadius: 32,
               ),
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                child: Stack(
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+            child: Stack(
+              children: <Widget>[
+                Column(
                   children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        AspectRatio(
-                          aspectRatio: 1.5,
-              child: CachedNetworkImage(
-                imageUrl: imageUrl,
-                fit:BoxFit.fill,
-                          ),
-                        ),
-                        Container(
-                          color:Colors.white,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Expanded(
-                                child: Container(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 16, top: 8, bottom: 8),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Text(
-                                          prodname,
-                                          textAlign: TextAlign.left,
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.w500,
-                                            fontSize: 22,
-                                          ),
-                                        ),
-                                        Padding(
-                                          padding:
-                                              const EdgeInsets.only(top: 4),
-                                          child: Row(
-                                            children: <Widget>[
-                                              Text(
-                                                'Go to',
-                                                style: TextStyle(
-                                                    fontSize: 14,
-                                                    color: Colors.grey
-                                                        .withOpacity(0.8)),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Padding(
+                    AspectRatio(
+                      aspectRatio: 1.5,
+                      child: CachedNetworkImage(
+                        imageUrl: imageUrl,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Container(
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              child: Padding(
                                 padding: const EdgeInsets.only(
-                                    right: 16, top: 8),
+                                    left: 16, top: 8, bottom: 8),
                                 child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.center,
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.end,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget>[
                                     Text(
-                                      '>>',
+                                      prodname,
                                       textAlign: TextAlign.left,
                                       style: TextStyle(
                                         fontWeight: FontWeight.w500,
                                         fontSize: 22,
                                       ),
                                     ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 4),
+                                      child: Row(
+                                        children: <Widget>[
+                                          Text(
+                                            'Go to',
+                                            style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.grey
+                                                    .withOpacity(0.8)),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
                                   ],
                                 ),
                               ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Positioned(
-                      top: 8,
-                      right: 8,
-                      child: Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(32.0),
-                          ),
-                          onTap: () {},
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(),
                             ),
                           ),
-                        ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16, top: 8),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Text(
+                                  '>>',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 22,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
+                    ),
                   ],
                 ),
-              ),
+                Positioned(
+                  top: 8,
+                  right: 8,
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(32.0),
+                      ),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        );
+        ),
+      ),
+    );
   }
 }
--- lib/pages/sendMoney/confirmdetails.dart
+++ lib/pages/sendMoney/confirmdetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
 import 'package:proto/constants.dart';
@@ -23,9 +24,10 @@ class VerifyPaymentCard extends StatelessWidget {
     Box userBox;
     userBox = Hive.box(Constants.UserBoxName);
     int currentBalance = userBox.get(Constants.BalanceStore);
-    return Card(
-      elevation: 10,
-      child: Container(
+    return DraggableCard(
+      child: AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          curve: Curves.fastLinearToSlowEaseIn,
           padding: EdgeInsets.only(top: 10.0),
           color: Colors.white,
           width: MediaQuery.of(context).size.width * 0.7,
@@ -175,18 +177,92 @@ class ConfirmPage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: LongPressDraggable(
-            childWhenDragging: Container(),
-            feedback: VerifyPaymentCard(
-                amount: (amount),
-                fees: fees,
-                receiverPhone: receiverPhone,
-                receiverName: receiverName),
-            child: VerifyPaymentCard(
-                amount: (amount),
-                fees: fees,
-                receiverPhone: receiverPhone,
-                receiverName: receiverName)),
+        child: VerifyPaymentCard(
+            amount: (amount),
+            fees: fees,
+            receiverPhone: receiverPhone,
+            receiverName: receiverName),
+      ),
+    );
+  }
+}
+
+class DraggableCard extends StatefulWidget {
+  final Widget child;
+
+  DraggableCard({this.child});
+
+  @override
+  _DraggableCardState createState() => _DraggableCardState();
+}
+
+class _DraggableCardState extends State<DraggableCard>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  var _dragAlignment = Alignment.center;
+
+  Animation<Alignment> _animation;
+
+  final _spring = const SpringDescription(
+    mass: 7,
+    stiffness: 1200,
+    damping: 0.7,
+  );
+
+  double _normalizeVelocity(Offset velocity, Size size) {
+    final normalizedVelocity = Offset(
+      velocity.dx / size.width,
+      velocity.dy / size.height,
+    );
+    return -normalizedVelocity.distance;
+  }
+
+  void _runAnimation(Offset velocity, Size size) {
+    _animation = _controller.drive(
+      AlignmentTween(
+        begin: _dragAlignment,
+        end: Alignment.center,
+      ),
+    );
+
+    final simulation =
+        SpringSimulation(_spring, 0.0, 1.0, _normalizeVelocity(velocity, size));
+
+    _controller.animateWith(simulation);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController.unbounded(vsync: this)
+      ..addListener(() => setState(() => _dragAlignment = _animation.value));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onPanStart: (details) => _controller.stop(canceled: true),
+      onPanUpdate: (details) => setState(
+        () => _dragAlignment += Alignment(
+          details.delta.dx / (size.width / 2),
+          details.delta.dy / (size.height / 2),
+        ),
+      ),
+      onPanEnd: (details) =>
+          _runAnimation(details.velocity.pixelsPerSecond, size),
+      child: Align(
+        alignment: _dragAlignment,
+        child: Card(
+          child: widget.child,
+        ),
       ),
     );
   }
